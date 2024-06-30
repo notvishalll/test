68a374ba26df81466790dee500c2104de0da90d3
Vishal <vishalcancode@gmail.com>
2024-06-30 15:10:19 +0530

    better

--- one.js
+++ one.js
@@ -1,7 +1,16 @@
 // simple function 
 
 function greet(user){
-    return console.log('hello,'+user)
+    return console.log('Hello,'+user)
 }
 
-greet("Vishal")
+function verify(user,age){
+    if (age>17){
+	    greet(user)
+}else{
+console.log(`You aren't eligible, ${user}`)
+}
+}
+
+verify("Vishal",18)
+verify("Naitik",15)
